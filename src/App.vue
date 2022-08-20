635eab26ee5ce844a91b90fa355bacee185bfcb3
Wes Rupert <wesr@outlook.com>
2022-08-19 19:49:03 -0700

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,10 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
-
-export default defineComponent({
-  name: "App",
-  components: {
-    HelloWorld,
-  },
-});
+export default defineComponent({ name: "App" });
 </script>
 
 <style lang="scss">
